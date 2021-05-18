798778d4682fdd1b6c5cc9101b296e403abff132
kannappan <kannappanchidambaram@gmail.com>
2021-05-18 06:18:45 +0530

    RainWaterTrapped Prob/solution

--- src/problems/RainWaterTrapped.java
+++ src/problems/RainWaterTrapped.java
@@ -1,5 +1,7 @@
 package problems;
 
+import java.util.ArrayList;
+
 public class RainWaterTrapped {
 
     public static void main(String[] args)
@@ -11,23 +13,28 @@ public class RainWaterTrapped {
 
     public static int trap(int[] A)
     {
-        int left=0,right=0,result=0;
+        int result=0;
+
+        int[] left = new int[A.length];
+        int[] right = new int[A.length];
+        left[0]= A[0];
+
+
+        for(int i=1;i<A.length;i++)
+        {
+            left[i]=Math.max(left[i-1],A[i]);
+        }
+
+        right[A.length-1]=A[A.length-1];
+        for(int i=A.length-2;i>=0;i--)
+        {
+            right[i]=Math.max(right[i+1],A[i]);
+        }
+
         for(int i=1;i<A.length-1;i++)
         {
 
-            left=A[i];
-            for(int j=0;j<i;j++)
-            {
-                left = Math.max(A[j],left);
-            }
-
-            right=A[i];
-            for(int k=i+1;k<A.length;k++)
-            {
-                right = Math.max(A[k],right);
-            }
-
-            result += Math.min(left,right)-A[i];
+            result += Math.min(left[i],right[i])-A[i];
 
         }
 
